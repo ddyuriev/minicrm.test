17f50d9eab4957b25ecc72c5d9bbdb65cf4a22c9
dy <therion@mail.ru>
2025-12-08 02:51:45 +0300

    remove create_files_table migration, add spatie/laravel-medialibrary

--- app/Models/Ticket.php
+++ app/Models/Ticket.php
@@ -4,9 +4,15 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Spatie\MediaLibrary\HasMedia;
+use Spatie\MediaLibrary\InteractsWithMedia;
 
-class Ticket extends Model
+class Ticket extends Model implements HasMedia
 {
-    use HasFactory;
+    use HasFactory, InteractsWithMedia;
+
+    protected $fillable = [
+        'customer_id', 'topic', 'text', 'status', 'date_of_response'
+    ];
 
 }
