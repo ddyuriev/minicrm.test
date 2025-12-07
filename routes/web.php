e38defc07989ba318e2c447eb4aa13958a00e9f5
dy <therion@mail.ru>
2025-12-07 20:06:43 +0300

    add initial models and controllers

--- routes/web.php
+++ routes/web.php
@@ -2,5 +2,10 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\WidgetController;
+use App\Http\Controllers\TicketController;
 
 Route::get('/widget', [WidgetController::class, 'show'])->name('widget.show');
+
+
+Route::get('/ticket', [TicketController::class, 'index'])->name('ticket.index');
+Route::get('/ticket/{id}', [TicketController::class, 'show'])->name('ticket.show');
